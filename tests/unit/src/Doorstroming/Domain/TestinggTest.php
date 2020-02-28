8598bbcd0bcd0887cb05c747bb608651f3242e7e
Mark Meijerman <mark@future500.nl>
2020-02-28 19:13:00 +0100

    codeception

---
+++ tests/unit/src/Doorstroming/Domain/TestinggTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mark\Doorstroming\Domain;
+
+use PHPUnit\Framework\TestCase;
+
+class TestinggTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function initialTest()
+    {
+        $this->assertTrue(true);
+    }
+}
